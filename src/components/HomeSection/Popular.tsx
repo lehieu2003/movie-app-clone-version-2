9db2edb7e0f9e3526e7dc31f10a2ebae99d7ab42
hieu <lehieunghiahanh76@gmail.com>
2025-01-12 13:11:39 +0700

    comment getMenuActive in List.tsx(Movie)

--- src/components/HomeSection/Popular.tsx
+++ src/components/HomeSection/Popular.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+
 // Services
 import { getPopular } from 'services/MovieApi'
 // Interfaces
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'selector',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
